2ef9d559841b1a794ba825eae6d3f1eef0b810b2
headdetect <headdetective2@gmail.com>
2013-07-16 12:29:31 -0600

    Correct implementation of configuration

--- CLI/Utils/Paramaters.cs
+++ CLI/Utils/Paramaters.cs
@@ -9,23 +9,17 @@ namespace ComputerRemote.CLI.Utils
     public class Paramaters
     {
 
-
+        private static Configuration config = new Configuration("options.json");
 
         static Paramaters()
         {
-            Configuration config = new Configuration("options.json");
-
             if (!config.KeyExists("Debug"))
                 config["Debug"] = new ConfigBlob("Debug", true);
 
             if (!config.KeyExists("Multicast"))
                 config["Multicast"] = new ConfigBlob("Multicast", true);
 
-            DebugEnabled = (bool)config["Debug"];
-            Multicating = (bool)config["Multicast"];
-
             config.Save();
-
         }
 
 
@@ -35,7 +29,18 @@ namespace ComputerRemote.CLI.Utils
         /// <value>
         ///   <c>true</c> if debug OUTPUT is enabled; otherwise, <c>false</c>.
         /// </value>
-        public static bool DebugEnabled { get; set; }
+        public static bool DebugEnabled
+        {
+            get
+            {
+                return (bool)config["Debug"];
+            }
+            set
+            {
+                config["Debug"] = value;
+                config.Save();
+            }
+        }
 
 
         /// <summary>
@@ -44,6 +49,17 @@ namespace ComputerRemote.CLI.Utils
         /// <value>
         ///   <c>true</c> if server multicasting is enabled; otherwise, <c>false</c>.
         /// </value>
-        public static bool Multicating { get; set; }
+        public static bool Multicating       
+        {
+            get
+            {
+                return (bool)config["Multicast"];
+            }
+            set
+            {
+                config["Multicast"] = value;
+                config.Save();
+            }
+        }
     }
 }
